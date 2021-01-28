1e7c2738007920c5d04a91b2efd18d49ea64742f
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-01-28 12:50:39 +0530

    Router setup done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', require('./routes'));
+
 app.listen(port, err => {
     if(err)
         console.log(`Error in running the server: ${err}`);
