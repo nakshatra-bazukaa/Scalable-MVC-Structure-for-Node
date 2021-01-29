1edf262067b076d704db8129b59e68085ac96fc6
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-01-29 07:59:45 +0530

    css added

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(expressLayouts);
 app.use(express.static('./assets'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes'));
 
