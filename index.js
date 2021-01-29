443bf78292c63c8ec151d0d214f5754ef8e9a612
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-01-29 08:43:18 +0530

    MongoDb added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const db = require('./config/mongoose');
+
 const app = express();
 
 const port = 8000;
