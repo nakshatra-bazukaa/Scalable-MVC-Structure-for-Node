e3bab44fa926e1ea43d0193a3093983e9c23f510
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-02-01 21:14:04 +0530

    Some bugs fixed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,18 +1,10 @@
 const User = require('../models/user');
 
 module.exports.profile = (req, res) => {
-    if(req.cookies.user_id)
-        User.findById(req.cookies.user_id, (err, user) => {
-            if(user)
-                return res.render('users.ejs', {
-                    title: 'User Page',
-                    content: 'User data will be shown here'
-                });
-            else
-                return res.redirect('/user/sign-in'); 
-        });
-    else
-        return res.redirect('/user/sign-in');  
+    return res.render('users', {
+        title: 'User Profile',
+        content: 'User data will be shown here'
+    })  
 }
 
 module.exports.sign_in = (req, res) => {
